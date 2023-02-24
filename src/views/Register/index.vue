5eba0e0657e202e9bda701917403140a8f656e7f
Demon-n <2993188146@qq.com>
2023-02-24 17:47:52 +0800

    实现注册跳转到登录页

--- src/views/Register/index.vue
+++ src/views/Register/index.vue
@@ -2,29 +2,29 @@
   <div class="user-form">
     <a-form :model="formState" name="basic" :label-col="{ span: 8 }" :wrapper-col="{ span: 16 }" autocomplete="off"
       @finish="onFinish" @finishFailed="onFinishFailed">
-      <a-form-item label="Username" name="username" :rules="[{ required: true, message: 'Please input your username!' }]">
+      <a-form-item label="用户名" name="username" :rules="[{ required: true, message: 'Please input your username!' }]">
         <a-input v-model:value="formState.username" />
       </a-form-item>
 
-      <a-form-item label="Password" name="password" :rules="[{ required: true, message: 'Please input your password!' }]">
+      <a-form-item label="密码" name="password" :rules="[{ required: true, message: 'Please input your password!' }]">
         <a-input-password v-model:value="formState.password" />
       </a-form-item>
 
-      <a-form-item label="Email" name="email" :rules="[{ required: true, message: 'Please input your email!' }]">
+      <a-form-item label="邮箱" name="email" :rules="[{ required: true, message: 'Please input your email!' }]">
         <a-input v-model:value="formState.email" />
       </a-form-item>
 
       <a-form-item name="remember" :wrapper-col="{ offset: 8, span: 16 }">
-        <a-checkbox v-model:checked="formState.remember">Remember me</a-checkbox>
+        <a-checkbox v-model:checked="formState.remember">记住密码</a-checkbox>
       </a-form-item>
 
       <a-form-item :wrapper-col="{ offset: 8, span: 16 }">
-        <a-button type="primary" html-type="submit">Submit</a-button>
+        <a-button type="primary" html-type="submit">注册</a-button>
       </a-form-item>
     </a-form>
   </div>
 </template>
-<script lang="ts">
+<script lang="ts" setup>
 import { defineComponent, reactive } from 'vue';
 import { reqRegister } from '../../api';
 import { useRouter } from 'vue-router';
@@ -32,51 +32,43 @@ import { message } from 'ant-design-vue';
 
 const router = useRouter()
 
-
 interface FormState {
   username: string;
   password: string;
   email: string
   remember: boolean;
 }
-export default defineComponent({
-  setup() {
-    const formState = reactive<FormState>({
-      username: '',
-      password: '',
-      email: '',
-      remember: true,
-    });
-    
-    const onFinish = (values: any) => {
-      let params = {
-        username: formState.username,
-        password: formState.password,
-        email: formState.email,
-      }
-      reqRegister(params).then((response) => {
-        if (response.status === 0) {
-          message.warn("恭喜您，注册成功,请登录！！！！")
-          console.log("我是toLogin方法@@@@", router)
-        }
-        else {
-          message.warn(response.message)
-        }
+const formState = reactive<FormState>({
+  username: '',
+  password: '',
+  email: '',
+  remember: true,
+});
+
+const onFinish = (values: any) => {
+  let params = {
+    username: formState.username,
+    password: formState.password,
+    email: formState.email,
+  }
+  reqRegister(params).then((response) => {
+    if (response.status === 0) {
+      message.warn("恭喜您，注册成功,请登录！！！！")
+      console.log("我是toLogin方法@@@@", router)
+      router.push({
+        path: '/'
       })
     }
+    else {
+      message.warn(response.message)
+    }
+  })
+}
 
-    const onFinishFailed = (errorInfo: any) => {
-      console.log('Failed:', errorInfo);
-    };
+const onFinishFailed = (errorInfo: any) => {
+  console.log('Failed:', errorInfo);
+};
 
-    return {
-      formState,
-      onFinish,
-      onFinishFailed,
-    };
-
-  },
-});
 </script>
 <style>
 body {
